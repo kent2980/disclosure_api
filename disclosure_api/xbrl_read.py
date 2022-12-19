fc8b032a7587707f724aeac97ffe68b1fefe177a
kent <kent2980@gmail.com>
2022-12-20 00:38:20 +0900

    commit

--- disclosure_api/xbrl_read.py
+++ disclosure_api/xbrl_read.py
@@ -11,6 +11,8 @@ from pathlib import Path
 import os
 import requests
 from datetime import datetime
+import csv
+import mojimoji
 
 class MyException(Exception):
     
@@ -39,7 +41,7 @@ class XbrlRead:
     def label_df(self) -> DataFrame:
         # XBRLを読み込む
         df = self.to_dataframe()
-        
+        df.to_csv('D:/CSV/dataframe.csv')
         # データ構造のみコピー
         master_df = df[:0]
         master_df['label'] = None
@@ -85,6 +87,9 @@ class XbrlRead:
     
     def to_dataframe(self) -> DataFrame:
         
+        dict_columns = ['reporting_date', 'code', 'period', '財表式別区分', '期区分', '連結・非連結区分', \
+            '報告区分', '報告詳細区分', 'start_date', 'end_date', 'instant_date', \
+            'namespace', 'element', 'context', 'unitref', 'format', 'numeric']
         df = None
         date_df = self.__context_date_df()
         company_datas = self.__Company_Data()
@@ -102,11 +107,6 @@ class XbrlRead:
                     # nonnumericタグを抽出                    
                     nonnumeric_tag = soup.find('ix:nonnumeric')
                     
-                    # 書類属性を取得
-                    doc_namespace = re.sub(":.*$", "", nonnumeric_tag.get('name'))
-                    doc_element = re.sub("^.*:", "", nonnumeric_tag.get('name'))
-                    doc_contextref = nonnumeric_tag.get('contextref')
-                    
                     # nonfractionタグを抽出
                     nonfraction_tag = soup.findAll('ix:nonfraction')
                     
@@ -118,47 +118,40 @@ class XbrlRead:
                                 raise XbrlValueNoneException("text_value")
                             
                             # 空の辞書を生成
-                            dict_tag = {}
+                            dict_tag = dict.fromkeys(dict_columns, None)
                             
                             # 名前空間、属性、コンテキストを取得
                             tag_namespace = re.sub(":.*$", "", tag.get('name'))
                             tag_element = re.sub("^.*:", "", tag.get('name'))
-                            tag_contextref = tag.get('contextref')                                 
+                            tag_contextref = tag.get('contextref')                                                             
                             
-                            # 過去のデータを参照した場合は例外発生
-                            if re.compile("Current").match(tag_contextref) is None:
-                                raise NotCurrentDurationError()
-                            
+                            # 開始日と終了日を取得
+                            dict_tag['start_date'] = date_df[date_df['id'] == tag_contextref]['start_date'].iloc[-1]
+                            dict_tag['end_date'] = date_df[date_df['id'] == tag_contextref]['end_date'].iloc[-1]
+                            dict_tag['instant_date'] = date_df[date_df['id'] == tag_contextref]['instant_date'].iloc[-1]
+
                             # 会社情報を登録
                             dict_tag['reporting_date'] = company_datas['reporting_date']
                             dict_tag['code'] = company_datas['code']
+                            dict_tag['period'] = company_datas['period']
                             
-                            # 開始日と終了日を取得
-                            match tag_contextref:
-                                #当四半期累計期間
-                                case "CurrentYTDDuration": 
-                                    dict_tag['start_date'] = date_df[date_df['id'].str.contains("CurrentAccumulatedQ[1-4]{1}Duration_.*$")]['start_date'].iloc[-1]
-                                    dict_tag['end_date'] = date_df[date_df['id'].str.contains("CurrentAccumulatedQ[1-4]{1}Duration_.*$")]['end_date'].iloc[-1]
-                                    dict_tag['instant_date'] = date_df[date_df['id'].str.contains("CurrentAccumulatedQ[1-4]{1}Duration_.*$")]['instant_date'].iloc[-1] 
-                                #当四半期会計期間
-                                case "CurrentQuarterInstant": 
-                                    dict_tag['start_date'] = date_df[date_df['id'].str.contains("CurrentAccumulatedQ[1-4]{1}Instant_.*$")]['start_date'].iloc[-1]
-                                    dict_tag['end_date'] = date_df[date_df['id'].str.contains("CurrentAccumulatedQ[1-4]{1}Instant_.*$")]['end_date'].iloc[-1]
-                                    dict_tag['instant_date'] = date_df[date_df['id'].str.contains("CurrentAccumulatedQ[1-4]{1}Instant_.*$")]['instant_date'].iloc[-1]                                    
-                                case "CurrentYearDuration":
-                                    dict_tag['start_date'] = date_df[date_df['id'].str.contains("CurrentYearDuration_.*$")]['start_date'].iloc[-1]
-                                    dict_tag['end_date'] = date_df[date_df['id'].str.contains("CurrentYearDuration_.*$")]['end_date'].iloc[-1]
-                                    dict_tag['instant_date'] = date_df[date_df['id'].str.contains("CurrentYearDuration_.*$")]['instant_date'].iloc[-1]   
-                                #通常処理
-                                case _: 
-                                    dict_tag['start_date'] = date_df[date_df['id'] == tag_contextref]['start_date'].iloc[-1]
-                                    dict_tag['end_date'] = date_df[date_df['id'] == tag_contextref]['end_date'].iloc[-1]
-                                    dict_tag['instant_date'] = date_df[date_df['id'] == tag_contextref]['instant_date'].iloc[-1] 
-                                
-                            # 各要素を辞書に追加
-                            dict_tag['document_namespace'] = doc_namespace
-                            dict_tag['document_element'] = doc_element
-                            dict_tag['document_context'] = doc_contextref
+                            # 書類情報を登録
+                            file_code = str(info.filename).split("/")
+                            file_code = file_code[len(file_code)-1].split("-")
+                            if re.compile("Attachment").search(info.filename):
+                                dict_tag['財表式別区分'] = file_code[1]
+                                report_cat = file_code[3]
+                            else:
+                                report_cat = file_code[1]
+                            if len(report_cat) == 4:
+                                dict_tag['報告区分'] = report_cat[0:4]
+                            else:
+                                dict_tag['期区分'] = report_cat[0] if re.compile("a|s|q").match(report_cat[0]) else None
+                                dict_tag['連結・非連結区分'] = report_cat[1] if re.compile("c|n").match(report_cat[1]) else None
+                                dict_tag['報告区分'] = report_cat[2:6]
+                                dict_tag['報告詳細区分'] = report_cat[6:8]
+                            
+                            # 各項目を登録
                             dict_tag['namespace'] = tag_namespace
                             dict_tag['element'] = tag_element
                             dict_tag['context'] = tag_contextref
@@ -166,7 +159,7 @@ class XbrlRead:
                             
                             if len(tag.contents) != 0:
                                 dict_tag['format'] = re.sub("^.*:", "", tag.get('format'))
-                                dict_tag['numeric'] = Decimal(re.sub(",", "", tag.contents[0])) * 10 ** Decimal(tag.get('scale'))
+                                dict_tag['numeric'] = Decimal(re.sub(r"\D", "", tag.contents[0])) * 10 ** Decimal(tag.get('scale'))
                                 
                                 # 数値がマイナスの場合
                                 if tag.get('sign') == '-':
@@ -176,9 +169,9 @@ class XbrlRead:
                             list.append(dict_tag)
                         except TypeError as identifier:
                             pass
-                        except NotCurrentDurationError:
+                        except NotCurrentDurationError as identifier:
                             pass
-                        except XbrlValueNoneException:
+                        except XbrlValueNoneException as identifier:
                             pass
                     
                     # DataFrameに変換
@@ -275,81 +268,76 @@ class XbrlRead:
         
     def __context_date_df(self) -> DataFrame:
         
+        # 辞書のキーを定義
+        dict_keys = ['id', 'start_date', 'end_date', 'instant_date']
+        
         # 期間情報を格納する辞書
         date_list = []
         
         # XBRLを取得
-        with zipfile.ZipFile(self.xbrl_zip_path, mode='r') as zip_data:
-            
-            # サマリより共通データを読み取り
-            for info in zip_data.infolist():
-                    
-                if re.compile("^tse-.*ixbrl.htm$|^.*Summary/.*ixbrl.htm$").match(info.filename):    
+        with zipfile.ZipFile(self.xbrl_zip_path, mode='r') as zip_data:           
+            for info in zip_data.infolist():                    
+                if re.compile(".*ixbrl.htm$").search(info.filename):    
                     
                     # BeautifulSoupで読み込む
                     soup = bs(zip_data.read(info.filename), 'lxml')                    
                         
                     # context<会計期間>タグを抽出
                     for context in soup.find_all('xbrli:context'):
+                        # 格納用の辞書を作成
+                        tag_dict = dict.fromkeys(dict_keys, None)
+                        tag_dict['id'] = context.get('id')
+                        tag_dict['start_date'] = None if context.find('xbrli:startdate') is None else context.find('xbrli:startdate').text
+                        tag_dict['end_date'] = None if context.find('xbrli:enddate') is None else context.find('xbrli:enddate').text
+                        tag_dict['instant_date'] = None if context.find('xbrli:instant') is None else context.find('xbrli:instant').text
+
+                        # リストに追加
+                        date_list.append(tag_dict)
                         
-                        # idを取得
-                        context_id = context.get('id')
-                        context_id = re.sub("_.*$", "", context_id)
-                        
-                        try:
-                            if context is None:
-                                raise XbrlValueNoneException('xbrli:context')
-                            
-                            for context_data in context.find_all('xbrli:period'):
-                                
-                                if context_data is None:
-                                    raise XbrlValueNoneException('xbrli:period')
-                                
-                                # 対象期間の詳細を格納する辞書
-                                date_dict = {}    
-                                
-                                # 辞書にidを登録
-                                date_dict['id'] = context_id
-                                
-                                # 要素を取得
-                                start_date = context_data.find('xbrli:startdate')
-                                end_date = context_data.find('xbrli:enddate')
-                                instant_date = context_data.find('xbrli:instant')
-                                date_dict['start_date'] = None if start_date is None else start_date.text
-                                date_dict['end_date'] = None if end_date is None else end_date.text
-                                date_dict['instant_date'] = None if instant_date is None else instant_date.text
-                                
-                                if start_date is None and end_date is None and instant_date is None:
-                                    raise XbrlValueNoneException('all date tag')
-                                
-                                # 詳細データを辞書に格納
-                                date_list.append(date_dict)
-                        except XbrlValueNoneException as identifier:
-                            pass 
+        df = DataFrame(date_list)
                     
-        date_df = DataFrame(date_list).drop_duplicates(subset='id')
-        date_df.to_csv("D:/CSV/date_df.csv")
-        return date_df 
+        return df 
         
     def __Company_Data(self) -> dict:
         
-        tag_dict = {}
+        # 空の辞書を作成
+        tag_dict = dict.fromkeys(['reporting_date', 'name', 'code', 'period'], None)
         
+        # Zipファイルを展開する
         with zipfile.ZipFile(self.xbrl_zip_path, 'r') as zip_data:
-            
+            # ファイルを展開する
             for info in zip_data.infolist():
-                if re.compile("^tse-.*ixbrl.htm$|^.*Summary/.*ixbrl.htm$").match(info.filename):
-                    try:
-                        soup = bs(zip_data.read(info.filename), 'lxml')
-                        
-                        tag_dict['reporting_date'] = soup.find('ix:nonnumeric', \
-                            attrs={'name':['tse-ed-t:ReportingDateOfFinancialForecastCorrection','tse-ed-t:FilingDate']}).text
-                        tag_dict['reporting_date'] = datetime.strptime(tag_dict['reporting_date'], "%Y年%m月%d日").date().strftime("%Y-%m-%d")
-                        tag_dict['name'] = soup.find('ix:nonnumeric', attrs={'name':'tse-ed-t:CompanyName'}).text
-                        tag_dict['code'] = soup.find('ix:nonnumeric', attrs={'name':'tse-ed-t:SecuritiesCode'}).text[0:4]
-                    except AttributeError as identifier:
-                        pass
+                # 対象のファイルを抽出
+                if re.compile("tse-.*ixbrl.htm$|^.*Summary/.*ixbrl.htm$|^.*ixbrl.htm$").search(info.filename):
                     
+                    # BeautifulSoupでスクレイピング
+                    soup = bs(zip_data.read(info.filename), 'lxml')
+                    
+                    # 提出日
+                    if tag_dict['reporting_date'] is None:
+                        date_str = str(self.xbrl_zip_path).split("\\")[2]
+                        tag_dict['reporting_date'] = datetime.strptime(date_str, "%Y%m%d").strftime("%Y-%m-%d")
+                                            
+                    # 会社名
+                    if tag_dict['name'] is None:
+                        company_name = soup.find('ix:nonnumeric', attrs={'name': [re.compile('^.*CompanyName'), re.compile('^.*AssetManagerREIT'), re.compile('FilerNameInJapaneseDEI')]})
+                        tag_dict['name'] = company_name.text if company_name is not None else None
+                    
+                    # 銘柄コード
+                    if tag_dict['code'] is None:
+                        code = soup.find('ix:nonnumeric', attrs={'name': [re.compile('^.*SecuritiesCode'), re.compile('^.*SecurityCodeDEI')]})
+                        tag_dict['code'] = code.text[0:4] if code is not None else None
+
+                    # 会計期間
+                    if tag_dict['period'] is None:
+                        period = soup.find('ix:nonnumeric', attrs={'name':re.compile('^.*TypeOfCurrentPeriodDEI')})
+                        period = period.text if period is not None else None
+                        if period is not None:
+                            if re.compile('Q[0-9]{1}').search(period):
+                                tag_dict['period'] = int(re.compile("[0-9]{1}").search(period).group()) - 1 
+                            elif period == 'FY':
+                                tag_dict['period'] = 3
+                                
         return tag_dict
                     
 zip_path = Path("D:/ZIP/")
@@ -362,3 +350,9 @@ for zip_file in zip_list:
     df = play.label_df()    
     file_name = os.path.splitext(os.path.basename(zip_file))[0]
     df.to_csv(f'D:/CSV/label/{file_name}.csv')
+
+# zip_path = "D:\\ZIP\\20221014\\081220220930538875.zip"
+# play = XbrlRead(zip_path)
+# df = play.label_df()
+# file_name = os.path.splitext(os.path.basename(zip_path))[0]
+# df.to_csv(f'D:/CSV/label/{file_name}.csv')
