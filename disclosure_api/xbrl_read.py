bce57785bfe011abdf4a840662982c5f047d3910
kent2980 <kent2980@gmail.com>
2023-03-05 20:13:02 +0900

    commit

--- disclosure_api/xbrl_read.py
+++ disclosure_api/xbrl_read.py
@@ -1,17 +1,3 @@
-    """このプログラムは >> pip install git+https://github.com/kent2980/disclosure_api.git
-    からインストールできます。
-
-    Raises:
-        NoneXbrlZipPathSetting: _description_
-        XbrlValueNoneException: _description_
-        LinkListEmptyException: _description_
-        LinkListEmptyException: _description_
-        LinkListEmptyException: _description_
-
-    Returns:
-        _type_: _description_
-    """
-
 from bs4 import BeautifulSoup as bs
 from pandas import DataFrame
 import pandas as pd
